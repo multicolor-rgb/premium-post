64f8a9e4a424ace7d7625d19fe612d1cebb1204e
Multi <83913997+multicolor-rgb@users.noreply.github.com>
2022-10-15 18:27:30 +0200

    1.0

--- js/addinput.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('#jsform');
-
-if(form!==null){
-console.log('test');
-    form.querySelector('#jseditor').insertAdjacentHTML('beforebegin',`
-     <input type="text" name="password-premium" class="form-control my-2 premiumpassword" placeholder="Access Password">`)
-
-}
